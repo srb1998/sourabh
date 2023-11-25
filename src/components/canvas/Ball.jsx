37ee77ca767126b745caea8183a6599b087b78e8
srb1998 <svgupta@mitaoe.ac.in>
2023-11-25 17:21:57 +0530

    Changed tech sphere

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -12,7 +12,7 @@ import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
-  
+
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
