9bb978183547b2c4641c4ea35f40f585cb9c18df
srb1998 <svgupta@mitaoe.ac.in>
2023-11-26 10:31:11 +0530

    Fixed GA initialize

--- src/utils/ga.js
+++ src/utils/ga.js
@@ -1,7 +1,7 @@
 import ReactGA from 'react-ga';
 
 export const initGA = () => {
-  ReactGA.initialize(import.meta.env.VITE_GA_KEY); // Replace 'your-measurement-id' with your actual Measurement ID
+  ReactGA.initialize([{trackingId:import.meta.env.VITE_GA_KEY}]); // Replace 'your-measurement-id' with your actual Measurement ID
 };
 
 export const logPageView = () => {
